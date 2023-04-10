f7fe4c6f1ce9304ac4a90ae025518370a166a307
sebas-baltor <baltazarosebas@gmail.com>
2023-04-09 19:38:29 -0600

    changing the main colors of the application

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,14 @@ module.exports = {
       colors:{
         "primary":"#1B2C62",
         "primary-contrast":"#4B5C92",
-        "secondary":"#F5626C",
-        "secondary-contrast":"#F2535E",
-        "light":"#E8E9EC"
+        // "secondary":"#F5626C",
+        // "secondary-contrast":"#F2535E",
+        "light":"#F5F8FF",
+        // "primary":"#126678",
+        // "primary-contrast":"#047683",
+        "secondary":"#BDFF00",
+        "secondary-contrast":"#C5FF1E",
+        // "light":"#F8F8F8"
       }
     },
   },
