60f4ff19bb263b9b2a8a694bcedd614c27836c2a
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-04-13 12:52:33 -0600

    testing a get method and the time this take to update

--- src/app/api/suscribe/route.ts
+++ src/app/api/suscribe/route.ts
@@ -1,28 +1,39 @@
 import Suscriber from "@/models/Suscriber";
 import dbConnect from "@/utils/dbConnect";
 import { NextResponse, NextRequest } from "next/server";
+import next from "next/types";
 export async function POST(request: NextRequest) {
-    try {
-        await dbConnect();
-        const { name, email, budget, message } = await request.json();
-        let suscriberStored = await Suscriber.find({ email })
-        if (suscriberStored !== null) {
-            return NextResponse.json({ message: "Hemos recibido tu mensaje y en unos dias nos pondremos en contacto con tigo, gracias", status: 201 });
-        }
-        let suscriber = new Suscriber({
-            name,
-            email,
-            budget,
-            message
-        })
-        await suscriber.save();
-        return NextResponse.json({ saved: suscriber, message: "success update", status: 200 });
+  try {
+    await dbConnect();
+    const { name, email, budget, message } = await request.json();
+    let suscriberStored = await Suscriber.find({ email });
+    if (suscriberStored !== null) {
+      return NextResponse.json({
+        message:
+          "Hemos recibido tu mensaje y en unos dias nos pondremos en contacto con tigo, gracias",
+        status: 201,
+      });
     }
-    catch (err) {
-        console.dir(err);
-        return NextResponse.json({ message: "something went wrong", status: 500 })
-    }
+    let suscriber = new Suscriber({
+      name,
+      email,
+      budget,
+      message,
+    });
+    await suscriber.save();
+    return NextResponse.json({
+      saved: suscriber,
+      message: "success update",
+      status: 200,
+    });
+  } catch (err) {
+    console.dir(err);
+    return NextResponse.json({ message: "something went wrong", status: 500 });
+  }
 }
-export async function GET(request:NextRequest){
-    return NextResponse.json("hola mundo")
+export async function GET(request: NextRequest) {
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((res) => res.json())
+    .then((data) => NextResponse.json(data))
+    .catch((err) => NextResponse.json(err));
 }
