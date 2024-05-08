c5fdba1abd1c7c98137ba87b28092f33a13327b9
Sebastián Baltazar <baltazarosebas@gmail.com>
2024-05-08 15:46:52 -0600

    añadiendo los assets a experiencias

--- src/components/home/Experience/Experience.tsx
+++ src/components/home/Experience/Experience.tsx
@@ -60,9 +60,9 @@ export default function Experience() {
           <TypingText title="Experience" />
 
           <ol className="border-l-2 border-secondary ml-5 mt-8 relative">
-            <TimelineItem date="2022" description="Years later, my curiosity has flourished, and I've challenged myself to learn all I can. This year, a company contacted me to design and implement the frontend for certain parts of their application." title="EXA Solutions" imgSrc="https://www.epl.ca/wp-content/uploads/sites/18/2020/12/CodingSnowflakes_Dec2020_BlogCard_890x445.png"/>
-            <TimelineItem date="2023" description="I began as a backend developer with .NET, creating a series of financial calculators. Later, I transitioned to experimenting with different technologies, including Windows Forms and Xamarin, all using C#." title="ASFIES" imgSrc="https://www.epl.ca/wp-content/uploads/sites/18/2020/12/CodingSnowflakes_Dec2020_BlogCard_890x445.png"/>
-            <TimelineItem date="2024" description="I joined the team as a full-stack developer, leveraging my knowledge from the .NET ecosystem. Quickly adapting, I transitioned to PHP, mastering Laravel and Livewire. Additionally, I deployed an application with Azure." title="FAW ELAM" imgSrc="https://www.epl.ca/wp-content/uploads/sites/18/2020/12/CodingSnowflakes_Dec2020_BlogCard_890x445.png"/>
+            <TimelineItem date="2022" description="Years later, my curiosity has flourished, and I've challenged myself to learn all I can. This year, a company contacted me to design and implement the frontend for certain parts of their application." title="EXA Solutions" imgSrc="/assets/axa-education.jpg"/>
+            <TimelineItem date="2023" description="I began as a backend developer with .NET, creating a series of financial calculators. Later, I transitioned to experimenting with different technologies, including Windows Forms and Xamarin, all using C#." title="ASFIES" imgSrc="/assets/nft-web-gallery.jpg"/>
+            <TimelineItem date="2024" description="I joined the team as a full-stack developer, leveraging my knowledge from the .NET ecosystem. Quickly adapting, I transitioned to PHP, mastering Laravel and Livewire. Additionally, I deployed an application with Azure." title="FAW ELAM" imgSrc="/assets/web-plant.jpg"/>
             <TimelineItem date="now" description="I started a proyect to become the next ERP for all the intern process for the company with React and Livewire" title="Fflowing"/>
           </ol>
 
